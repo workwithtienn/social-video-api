a50ddc5f6a6aa48cec71f22eed684e39ad1e3abb
V Tiếnn <workwithtienn@gmail.com>
2025-11-24 13:36:23 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'TikTok & Douyin Downloader',
+  description: 'Tải video không watermark',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="vi">
+      <body>{children}</body>
+    </html>
+  )
+}
